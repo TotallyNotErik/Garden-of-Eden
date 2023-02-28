3658bcf9b59c3da734324b54ad496620c0f6299d
TotallyNotErik <Choverik@gmail.com>
2023-02-28 16:02:56 -0700

    Made Birds Scared & Fixed Camera Movement bugs + Web Build

--- Garden of Eden/Assets/Scripts/BirdScript.cs
+++ Garden of Eden/Assets/Scripts/BirdScript.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Transform target;
+    private bool scared = false;
+    private float birdtimer = 0f;
+    private float birdreturn = 15f;
+    private Vector3 pos;
+    public GameObject bird;
 
     void Start()
     {
         target = GameObject.Find("Player").transform;
+        pos = this.transform.position;
     }
 
     void LateUpdate()
     {
+        if (scared)
+            this.transform.position += this.transform.rotation * new Vector3(0, 2, 1) * Time.fixedDeltaTime;
+        else
+            this.transform.LookAt(target);
 
-	    this.transform.LookAt(target);
+        if (Time.time > birdtimer + birdreturn && scared) 
+        {
+            GameObject newbird = Instantiate(bird, pos, this.transform.rotation) as GameObject;
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player" || other.gameObject.name == "Bullet(Clone)")
+        {
+            scared = true;
+            birdtimer = Time.time;
+        }
 
     }
 }
--- Garden of Eden/Assets/Scripts/CameraMovementBehavior.cs
+++ Garden of Eden/Assets/Scripts/CameraMovementBehavior.cs
@@ -20,11 +20,7 @@ public class CameraMovementBehavior : MonoBehaviour
     {
         camInput = -Input.GetAxis("Vertical") * gameManager.sensitivity *rotateSpeed * Time.deltaTime;
         Vector3 rotation = Vector3.right * camInput;
-        if(transform.eulerAngles.x >= 360-45 || transform.eulerAngles.x <= 30)
+        if((transform.localRotation * Quaternion.Euler(rotation)).x > -.40 && (transform.localRotation * Quaternion.Euler(rotation)).x < 0.27)
             transform.localRotation = transform.localRotation * Quaternion.Euler(rotation);
-        else if(transform.eulerAngles.x < 360-45 && transform.eulerAngles.x > 180  )
-            transform.localRotation = Quaternion.Euler(360-45,0,0);
-        else if(transform.eulerAngles.x >= 30 && transform.eulerAngles.x < 180)
-            transform.localRotation = Quaternion.Euler(30,0,0);
     }
 }
